d1ff2da8cc6d2c5f868532e7045d2cb2d02182f5
Paulo Hercílio <paulo.hercilio2015@gmail.com>
2023-08-29 23:09:08 -0300

    feat: create a content struct and controll by writing in terminal

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use crossterm::event::{Event, KeyCode, KeyEvent};
 use crossterm::terminal::ClearType;
-use crossterm::{cursor, event, execute, terminal};
-use std::io::stdout;
+use crossterm::{cursor, event, execute, queue,terminal};
+use std::io::{self, stdout, Write};
 use std::time::Duration;
 
+const VERSION: &str = "0.0.1";
 struct CleanUp;
 
 impl Drop for CleanUp {
@@ -50,7 +51,7 @@ impl Editor {
         Ok(true)
     }
 
-    fn run(&self) -> crossterm::Result<bool> {
+    fn run(&mut self) -> crossterm::Result<bool> {
         self.output.refresh_screen()?;
         self.process_keypress()
     }
@@ -58,11 +59,18 @@ impl Editor {
 
 struct Output {
     win_size: (usize, usize),
+    editor_contents: EditorContents,
 }
 
 impl Output {
     fn new() -> Self {
-        Self
+        let win_size = terminal::size()
+            .map(|(x, y)| (x as usize, y as usize))
+            .unwrap();
+        Self {
+            win_size,
+            editor_contents: EditorContents::new(),
+        }
     }
 
     fn clear_screen() -> crossterm::Result<()> {
@@ -70,16 +78,93 @@ impl Output {
         execute!(stdout(), cursor::MoveTo(0, 0))
     }
 
-    fn draw_rows(&self) {
-        for _ in 0..24 {
-            println!("~\r");
+    fn draw_rows(&mut self) {
+        let screen_rows = self.win_size.1;
+        let screen_columns = self.win_size.0;
+        for i in 0..screen_rows {
+
+            if i == screen_rows / 3 {
+                let mut welcome = format!("Ser Editor ---- version {}", VERSION);
+                if welcome.len() > screen_columns {
+                    welcome.truncate(screen_columns)
+                }
+                let mut padding = (screen_columns - welcome.len()) /2;
+           
+                if padding != 0 {
+                    self.editor_contents.push('~');
+                    padding -= 1
+                }
+
+                for _ in 0..padding {
+                    self.editor_contents.push(' ');
+                }
+
+                self.editor_contents.push_str(&welcome);
+            } else {
+                self.editor_contents.push('~');
+            }
+
+            queue!(
+                self.editor_contents,
+                terminal::Clear(ClearType::UntilNewLine)
+            )
+            .unwrap();
+
+            if i < screen_rows - 1 {
+                self.editor_contents.push_str("\r\n");
+            }
         }
     }
 
-    fn refresh_screen(&self) -> crossterm::Result<()> {
-        Self::clear_screen()?;
+    fn refresh_screen(&mut self) -> crossterm::Result<()> {
+        queue!(self.editor_contents, 
+            cursor::Hide,
+            cursor::MoveTo(0, 0))?;
         self.draw_rows();
-        execute!(stdout(), cursor::MoveTo(0, 0))
+        queue!(
+            self.editor_contents, 
+            cursor::MoveTo(0, 0),
+            cursor::Show
+        )?;
+        self.editor_contents.flush()
+    }
+}
+
+struct EditorContents {
+    content: String,
+}
+impl EditorContents {
+    fn new() -> Self {
+        Self {
+            content: String::new(),
+        }
+    }
+
+    fn push(&mut self, c: char) {
+        self.content.push(c);
+    }
+
+    fn push_str(&mut self, string: &str) {
+        self.content.push_str(string)
+    }
+}
+
+impl Write for EditorContents {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        match std::str::from_utf8(buf) {
+            Ok(s) => {
+                self.content.push_str(s);
+                Ok(buf.len())
+            }
+            Err(_) => Err(io::ErrorKind::WriteZero.into()),
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        let out = write!(stdout(), "{}", self.content);
+        stdout().flush()?;
+        self.content.clear();
+        out
     }
 }
 
@@ -89,8 +174,9 @@ fn main() -> crossterm::Result<()> {
     // let mut buf = [0; 1];
     terminal::enable_raw_mode()?;
 
-    let editor = Editor::new();
+    let mut editor = Editor::new();
 
     while editor.run()? {}
+
     Ok(())
 }
